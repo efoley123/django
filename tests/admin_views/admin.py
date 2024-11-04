3270745b60cd402017d5ca5ff78f3471e85088ce
Rosy122333 <80847580+Rosy122333@users.noreply.github.com>
2024-11-04 10:37:45 -0800

    testing admin

--- tests/admin_views/admin.py
+++ tests/admin_views/admin.py
@@ -1,7 +1,7 @@
 import datetime
 from io import StringIO
 from wsgiref.util import FileWrapper
-
+#testing
 from django import forms
 from django.contrib import admin
 from django.contrib.admin import BooleanFieldListFilter
